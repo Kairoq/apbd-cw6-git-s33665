9a3fa0ff6eb108060ed00ea49dda1ea815d62952
Kairoq <maksik771@wp.pl>
2026-04-20 15:43:55 +0200

    Added classes: Room and Reservation

---
+++ Model/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apbd_cw5_git_s33665.Model;
+
+public class Reservation
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Room Id not provided")]
+    public int? RoomId { get; set; }
+
+    [Required(ErrorMessage = "Organizer name not provided")]
+    public string OrganizerName { get; set; }
+
+    [Required(ErrorMessage = "Topic not provided")]
+    public string Topic { get; set; }
+
+    [Required(ErrorMessage = "Date not provided")]
+    public DateOnly? Date { get; set; }
+
+    [Required(ErrorMessage = "Beginning time not provided")]
+    public TimeSpan? StartTime { get; set; }
+
+    [Required(ErrorMessage = "Ending time not provided")]
+    public TimeSpan? EndTime { get; set; }
+
+    [Required(ErrorMessage = "Status not provided")]
+    public string Status { get; set; }
+    
+}
---
+++ Model/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apbd_cw5_git_s33665.Model;
+
+public class Room
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name not provided")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Building Code not provided")]
+    public string BuildingCode { get; set; }
+
+    [Required(ErrorMessage = "Floor not provided")]
+    public int? Floor { get; set; }
+
+    [Required(ErrorMessage = "Capacity not provided")]
+    [Range(1, int.MaxValue, ErrorMessage = "Capacity must be greater than 0")]
+    public int? Capacity { get; set; }
+
+    [Required(ErrorMessage = "Provide projector availability information")]
+    public bool? HasProjector { get; set; }
+
+    [Required(ErrorMessage = "Provide status information")]
+    public bool? IsActive { get; set; }
+}
